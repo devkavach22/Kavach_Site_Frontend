f9cbc2e77bc950fa888106578d16afb76773ab9b
devkavach22 <developer@kavachglobal.com>
2026-02-24 11:44:09 +0530

    fix the build issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base:'/',
   plugins: [
+    react(), 
     tailwindcss(),
   ],
+  build: {
+    outDir: 'build',     // 👈 Output folder renamed
+    emptyOutDir: true    // clear old files
+  }
 })
